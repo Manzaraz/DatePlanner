42f635ab7bfd1089166497774467dbd46c311d54
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-04 16:42:46 -0300

    Initial Commit

---
+++ DatePlanner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  DatePlanner
+//
+//  Created by Christian Manzaraz on 04/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct DatePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
